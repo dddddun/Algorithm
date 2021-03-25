f547ee22ac66d6c0c0167a96892a25f03781f6b5
seonghyeonseon <leaf1993@naver.com>
2021-03-25 18:17:03 +0900

    stack_queue: Question02

---
+++ programmers/stack_queue/PG_stack_queue_Question02.py
@@ -0,0 +1,49 @@
+# 나의 풀이
+def solution(prices):
+    answer = []
+
+    for i in range(len(prices)):
+        cnt = 0
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                cnt += 1
+            else:
+                cnt += 1
+                break
+        answer.append(cnt)
+    return answer
+
+# 스택과 큐를 사용하지는 않았지만 개선된 코
+'''
+def solution(prices):
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
+    return answer
+'''
+
+# 스택을 사용한 풀이
+from collections import deque
+def solution(prices):
+    answer = []
+    prices = deque(prices)
+    while prices:
+        c = prices.popleft()
+
+        count = 0
+        for i in prices:
+            if c > i:
+                count += 1
+                break
+            count += 1
+
+        answer.append(count)
+
+    return answer
+
+# solution([1, 2, 3, 2, 3])
