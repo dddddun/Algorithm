2469b9bc3c87ad86590fdf312db2975c4d62c5ec
seonghyeonseon <leaf1993@naver.com>
2021-03-31 02:30:58 +0900

    heap: Question03

--- programmers/heap/PG_heap_Question02.py
+++ programmers/heap/PG_heap_Question02.py
@@ -1,19 +1,57 @@
+# 나의 풀이 실패
+'''
 import heapq
 def solution(jobs):
-    answer, start, end = 0, 0, 0
+    answer = []
     hq = []
     jobs.sort(key=lambda x: (x[0], x[1]))
-    print(jobs)
-    now = jobs.pop(0)
-    end = now[0] + now[1]
-    for job in jobs:
-        if job[0] <= end:
-            heapq.heappush(hq, job)
-    now = heapq.heappop(hq)
-    end = now[1]
+    first = jobs.pop(0)
+    end = first[1]
+    hq.append((end, first[0]))
+    # 좝스 안에 시작이 끝 보다 작은 애,들을 먼저 뽑아서 hq에 넣는다.
+    while jobs:
+        print(jobs)
+        i = 0
+        while max(j[0] for j in jobs) <= end:
+            if jobs[i][0] <= end:
+                job = jobs.pop(i)
+                heapq.heappush(hq, (job[1], job[0]))
+            else:
+                i += 1
 
+        while hq:
+            # 걸리는 시간 = end - start
+            hq_job = heapq.heappop(hq)
+            end = end + hq_job[0]
+            answer.append(end - hq_job[1])
+    print(answer)
 
     return answer
+'''
 
 
-solution([[0, 3], [3, 6], [1, 9], [4, 8], [2, 6]])
+# 해설
+from heapq import heappush, heappop
+
+
+def solution(jobs):
+    jobs.sort(reverse=True)
+    que = []
+    time, total_wait_run, length = 0, 0, len(jobs)
+    while jobs or que:
+        while jobs and jobs[-1][0] <= time:
+            start, running = jobs.pop()
+            heappush(que, (running, start))
+        if que:
+            running, start = heappop(que)
+            total_wait_run += time - start + running
+            time = time + running
+        else:
+            time = jobs[-1][0]
+    return total_wait_run // length
+
+
+
+# solution([[0, 3], [1, 9], [2, 6]])
+
+
