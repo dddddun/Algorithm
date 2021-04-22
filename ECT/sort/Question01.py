54968c1cf2cef3895ebe820f88da04ea2376ccfb
seonghyeonseon <leaf1993@naver.com>
2021-04-22 16:31:41 +0900

    ECT: sort_Question01

--- ECT/sort/Question01.py
+++ ECT/sort/Question01.py
@@ -4,9 +4,9 @@ def solution():
     for _ in range(n):
         students.append(list(input().split()))
 
-    sort_student = sorted(students, key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+    students.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
-    for student in sort_student:
+    for student in students:
         print(student[0])
 
 
