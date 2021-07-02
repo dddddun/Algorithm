5ee56d5ef23a606ef3eee5fcd9cf36413fc81d7e
seonghyeonseon <leaf1993@naver.com>
2021-07-02 20:06:13 +0900

    BackJoon: Question_15651

--- BackJoon/Brute_force/15651.py
+++ BackJoon/Brute_force/15651.py
@@ -1,24 +1,20 @@
-def permutaion(level, start):
+def permutaion(level):
     global n, m, arr, res, ch
     if level == m:
         print(' '.join(map(str, res)))
     else:
-        for i in range(start, n):
-            if not ch[i]:
-                ch[i] = True
-                res.append(arr[i])
-                permutaion(level + 1, i + 1)
-                res.pop()
-                ch[i] = False
+        for i in range(n):
+            res.append(arr[i])
+            permutaion(level + 1)
+            res.pop()
 
 
 def solution(N, M):
     global n, m, arr, res, ch
     n, m = N, M
     arr = [i for i in range(1, n + 1)]
-    ch = [False for _ in range(n)]
     res = list()
-    permutaion(0, 0)
+    permutaion(0)
 
 
 if __name__ == "__main__":
